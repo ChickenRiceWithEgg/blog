d585f9d835b7fe6fc9b390dd91d2cdbf17f9468a
xiejingwei <jingwei144@foxmail.com>
2020-05-15 15:11:10 +0800

    第一次提交

---
+++ src/test/java/com/xjw/doth/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjw.doth.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
